f91b5b59cbd80110eba2b58e09b43298f94c6887
Dave Rintoul <drintoul@gmail.com>
2023-12-14 20:56:04 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -4,3 +4,24 @@ import pandas as pd
 st.write("""
 # Rays Home Attendance Predictor
 """)
+
+st.sidebar.header('User Input Parameters')
+
+def user_input_features():
+
+  day = st.sidebar.toggle('Day Game')
+  opp = st.sidebar.dropdown()
+
+  data = {'day': day,
+          'opp': opp}
+
+  features = pd.DataFrame(data, index=[0])
+  return features
+
+df = user_input_features()
+
+st.subheader('User Input Parameters')
+st.write(df)
+
+
+  

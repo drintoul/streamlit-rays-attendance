6af9e0427fd64fc4240d6dfde29e8561e0ae2e88
Dave Rintoul <drintoul@gmail.com>
2023-12-15 09:00:02 -0500

    Update get_games.py

--- get_games.py
+++ get_games.py
@@ -3,25 +3,54 @@ def get_games(**kwargs):
     """
     
     import pandas as pd
+    from datetime import datetime
+    import re
+    
+    names = {'HOU': 'Houston Astros', 'COL': 'Colorado Rockies', 'SFG': 'San Francisco Giants', 
+         'CHW': 'Chicago White Sox', 'TOR': 'Toronto Blue Jays', 'BAL': 'Baltimore Orioles', 
+         'BOS': 'Boston Red Sox', 'KCR': 'Kansas City Royals', 'ARI': 'Arizona Diamondbacks',
+         'NYY': 'New York Yankees', 'MIA': 'Miami Marlins', 'LAD': 'Los Angeles Dodgers', 
+         'CLE': 'Cleveland Guardians', 'MIN': 'Minnesota Twins', 'DET': 'Detroit Tigers', 
+         'OAK': 'Oakland Athletics', 'LAA': 'Los Angeles Angels', 'TEX': 'Texas Rangers',
+         'SEA': 'Seattle Mariners', 'SDP': 'San Diego Padres', 'WSN': 'Washington Nationals',
+         'NYM': 'New York Mets', 'CHC': 'Chicago Cubs', 'ATL': 'Atlanta Braves',
+         'PHI': 'Phildelphia Phillies', 'CIN': 'Cincinnati Reds', 'STL': 'St. Louis Cardinals',
+         'MIL': 'Milwaukee Brewers', 'PIT': 'Pittsburg Pirates'}
     
     df = pd.DataFrame()
     year = kwargs.get('year')
     
+    # retrieve table
     dfs = pd.read_html(f'https://www.baseball-reference.com/teams/TBR/{year}-schedule-scores.shtml')
-    data = dfs[0]
-    
-    data = data[data['Attendance'] != 'Attendance']
-    data = data[['Date', 'Unnamed: 4', 'Opp' ,'Rank', 'GB', 'D/N', 'Attendance']]
-    data.rename(columns={'Unnamed: 4': 'HA'}, errors='ignore', inplace=True)
-    data['HA'].fillna('H', inplace=True)
+    df = dfs[0]
 
-    data['GB'] = data['GB'].str.replace('Tied', '0.0').str.replace('up ', '-')
-    data['GB'] = data['GB'].astype(float)
+    # format
+    df.drop(columns=['Gm#', 'Unnamed: 2', 'Tm', 'W/L', 'R', 'RA', 'Inn', 'W-L', 'Win', 'Loss', 'Save', 
+                 'Time', 'cLI', 'Streak', 'Orig. Scheduled'], inplace=True)
+    df = df[df['Rank'] != 'Rank']
+    df['GB'] = df['GB'].apply(lambda x: x.replace('Tied', '0.0').replace('up ', ''))
+    df['Opp'] = df['Opp'].map(names)
+    df['Year'] = year
+    df = df.rename(columns={'Unnamed: 4': 'H/A', 'Opp': 'Opponent'})
     
+    df = df[['Date', 'Year', 'Opponent', 'H/A', 'Rank', 'GB', 'D/N', 'Attendance']]
+    
+    return df
+
+
+import pandas as pd
+import time
+
+df = pd.DataFrame()
+
+for year in range(2013, 2024):
+    
+    if year == 2020:
+        continue
+
+    data = get_games(year=year)
     df = pd.concat([df, data], axis=0)
     
-    df['Rank'] = df['Rank'].shift(1)
-    df['GB'] = df['GB'].shift(1)
-    df = df[1:]
- 
-    return df
+    time.sleep(10)
+    
+df.to_csv('rays_attendance.csv', index=False)

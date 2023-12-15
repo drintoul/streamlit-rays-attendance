69df7eaa531a63ebe6ef6bb9be92e2637e14c70d
Dave Rintoul <drintoul@gmail.com>
2023-12-15 13:40:11 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -35,7 +35,7 @@ def user_input_features():
   daygame = st.sidebar.toggle('Day Game')
   div_rank = st.sidebar.slider('Ray\'s AL East Division Rank before game', min_value=1, max_value=5, value=1, step=1)
   gb = st.sidebar.slider('Games Behind Division Leader before game', min_value=-5.0, max_value=10.0, value=0.0, step=0.50)
-  st.sidebar.write('negative games behind = positive games ahead')
+  st.sidebar.write('negative games behind = games ahead')
   opponent = st.sidebar.selectbox('Opponent', all_teams, index=13) # default to LA Dodgers
 
   if opponent in al_east_teams:

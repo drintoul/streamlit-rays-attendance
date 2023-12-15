9077a8e885ab4800dccad533ee5c5fc5284fc0ac
Dave Rintoul <drintoul@gmail.com>
2023-12-15 12:28:09 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -88,8 +88,8 @@ data, features = user_input_features()
 
 df = pd.read_csv('rays_attendance.csv')
 
-max = df['attendance'].max()
-min = df['attendance'].min()
+max = df['Attendance'].max()
+min = df['Attendance'].min()
 
 df['al_east'] = df['Opponent'].apply(lambda x: True if x in al_east_teams else False)
 df['al_other'] = df['Opponent'].apply(lambda x: True if x in al_other_teams else False)

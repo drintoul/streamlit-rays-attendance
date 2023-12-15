f7b7c7e22d95854f0f5decf3c387a8dd80211cc4
Dave Rintoul <drintoul@gmail.com>
2023-12-14 21:07:14 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -5,6 +5,19 @@ st.write("""
 # Rays Home Attendance Predictor
 """)
 
+capacity = 31042
+
+al_east = ['Baltimore Orioles', 'Boston Red Sox', 'New York Yankees', 'Toronto Blue Jays', 'Tampa Bay Rays']
+al_other = ['Chicago White Sox', 'Cleveland Guardians', 'Detroit Tigers', 'Kansas City Royals', 'Minnesota Twins',
+            'Houston Astros', 'Los Angeles Angels', 'Oakland Athletics', 'Seattle Mariners', 'Texas Rangers']
+nl = ['Atlanta Braves', 'Miami Marlins', 'New York Mets', 'Philadelphia Phillies', 'Washington Nationals',
+      'Chicago Cubs', 'Cincinnati Reds', 'Milwaukee Brewers', 'Pittsburgh Pirates', 'St. Louis Cardinals',
+      'Arizona Diamond Backs', 'Coloroado Rockies', 'Los Angeles Dodgers', 'San Diego Padres', 'San Francisco Giants']
+
+all = al_east + al_other + nl
+
+st.write(all)
+
 st.sidebar.header('User Input Parameters')
 
 def user_input_features():

bbc5a51c9ce2bf4fe5ee3b8a14d3c198870a0127
Dave Rintoul <drintoul@gmail.com>
2023-12-14 23:37:17 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -83,7 +83,8 @@ regr.fit(X_train, y_train)
 
 y_pred = regr.predict(X_test)
 
-st.write("Estimated Error: +/- ", int(np.round(np.sqrt(mean_absolute_error(y_test, y_pred)),-2)))
-
 prediction = regr.predict(features_df.values)
 st.write('Prediction:', prediction)
+
+st.write("Estimated Error: +/- ", int(np.round(np.sqrt(mean_absolute_error(y_test, y_pred)),-2)))
+
